544addaba48bf6ec5504d8b4732f6e922c949f83
tarik <t.fertahi_etu@enset-media.ac.ma>
2021-06-05 18:40:26 +0100

    methode GetAllUsers

---
+++ respositories/users.js
@@ -0,0 +1,19 @@
+const { User } = require('../models')
+module.exports = {
+    getAllUsers() {
+    return User.findAll()
+    },
+    
+    getUsers(offset = 0, limit = 10) { },
+    getAdmins() { 
+        
+     },
+    getAuthors() { },
+    getGuests(){ },
+    getUser(id) { },
+    getUserByEmail(email) { },
+    addUser(user) { },
+    updateUser() { },
+    deleteUser() { },
+    // D'autres méthodes jugées utiles
+    }
