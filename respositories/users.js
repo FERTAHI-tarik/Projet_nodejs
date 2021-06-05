d1b16d7bf419603d164c2808e5403f42afa84d7a
tarik <t.fertahi_etu@enset-media.ac.ma>
2021-06-05 23:35:44 +0100

    manage Users

--- respositories/users.js
+++ respositories/users.js
@@ -1,19 +1,68 @@
+//const { where } = require('sequelize/types');
 const { User } = require('../models')
+const user = require('../models/user')
 module.exports = {
     getAllUsers() {
     return User.findAll()
     },
-    
-    getUsers(offset = 0, limit = 10) { },
-    getAdmins() { 
-        
+
+    //getUsers(offset = 0, limit = 10) { },
+
+    getAdmins(){ 
+        return User.findAll({
+            where : { 
+                role : "admin"
+            },
+        });
      },
-    getAuthors() { },
-    getGuests(){ },
-    getUser(id) { },
-    getUserByEmail(email) { },
-    addUser(user) { },
-    updateUser() { },
-    deleteUser() { },
+
+    getAuthors() {
+        return User.findAll({
+            where : { 
+                role : "author"
+            },
+        });
+     },
+
+    getGuests(){
+        return User.findAll({
+            where : { 
+                role : "guest"
+            },
+        });
+     },
+
+    getUser(id) {
+        return User.findOne({
+            where : { 
+                id : id
+            },
+        }); 
+    },
+
+
+    getUserByEmail(email) { 
+        return User.findOne({
+            where : { 
+                email : email
+            },
+        }); 
+    },
+
+    addUser(user) { 
+        return User.Create(user)
+    },
+
+    updateUser(user) { 
+        return User.update(user);
+    },
+
+    deleteUser(email) { 
+        User.destroy({
+            where: {
+                email: email
+            },
+        });
+    },
     // D'autres méthodes jugées utiles
     }
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,18 @@
 var express = require('express');
+const { use } = require('.');
 var router = express.Router();
 const userRepo = require('../respositories/users');
 
 
 /* GET users listing. */
 router.get('/', async function(req, res, next) {
-  res.send(await userRepo.getAllUsers())
+  //res.send(await userRepo.getAllUsers())
+  //res.send(await userRepo.getAdmins())
+  //res.send(await userRepo.getAuthors())
+  //res.send(await userRepo.getGuests())
+  //res.send(await userRepo.getUser(1))
+  //res.send(await userRepo.getUserByEmail("Roel68@yahoo.com"))
+
 });
 
 module.exports = router;
